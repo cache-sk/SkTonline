94a6c843307effbf0abef280312ddd4fe502a96d
cache-sk <cache.sk@gmail.com>
2022-12-06 16:59:18 +0100

    1.1.0 - pridanie historie pozerania, historie hladania a uprava vyberu streamu

--- plugin.video.sktonline/sktonline.py
+++ plugin.video.sktonline/sktonline.py
@@ -4,27 +4,31 @@
 # Created on: 6.8.2019
 # License: AGPL v.3 https://www.gnu.org/licenses/agpl-3.0.html
 
-import sys
+import sys, os, io
 import xbmc
 import xbmcgui
 import xbmcplugin
 import xbmcaddon
 import requests
 import traceback
+import json
 from bs4 import BeautifulSoup
 
 try:
     from urllib import urlencode
     from urlparse import parse_qsl
+    from xbmc import translatePath
 except ImportError:
     from urllib.parse import urlencode
     from urllib.parse import parse_qsl
+    from xbmcvfs import translatePath
 
 _url = sys.argv[0]
 _handle = int(sys.argv[1])
 
 _addon = xbmcaddon.Addon()
 _session = requests.Session()
+_profile = translatePath( _addon.getAddonInfo('profile'))
 
 BASE_URL = 'https://online.sktorrent.eu'
 HEADERS={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36', 'Referer': BASE_URL, 'Accept-Encoding': 'identity'}
@@ -33,6 +37,10 @@ SEARCH_DEFAULT_PARAMS = ['type=public','t=a','o=mr']
 PAGE_PARAM = 'page'
 SEARCH_PARAM = 'search_query'
 FIRST_PAGE = 1
+LAST_WATCHED = "last_watched"
+SEARCH_HISTORY = 'search_history'
+LAST_SEARCH = "last_search"
+MAX_HISTORY_SIZE = 20
 
 CATEGORIES = [
     {'msg':_addon.getLocalizedString(30201), 'url':'/videos'},
@@ -60,6 +68,83 @@ LISTS = [
 def get_url(**kwargs):
     return '{0}?{1}'.format(_url, urlencode(kwargs, 'utf-8'))
 
+def check_profile():
+    if not os.path.exists(_profile):
+        os.makedirs(_profile)
+
+def load_last_watched():
+    check_profile()
+    last_watched = []
+    fname = os.path.join(_profile, LAST_WATCHED)
+    try:
+        with io.open(fname, 'r', encoding='utf8') as file:
+            last_data = file.read()
+            last_watched = json.loads(last_data)
+    except Exception as e:
+        xbmc.log('Can\'t load '+LAST_WATCHED+'\n'+str(e),level=xbmc.LOGINFO)
+        traceback.print_exc()
+    return last_watched
+
+def store_last_watched(href, title, img):
+    last_watched = load_last_watched()
+    fname = os.path.join(_profile, LAST_WATCHED)
+    tpl = {'href':href,'title':title,'img':img}
+    if tpl in last_watched:
+        last_watched.remove(tpl)
+    last_watched = [tpl] + last_watched
+    last_watched = last_watched[:MAX_HISTORY_SIZE]
+    try:
+        with io.open(fname, 'w', encoding='utf8') as file:
+            try:
+                dump = json.dumps(last_watched).decode('utf8')
+            except AttributeError:
+                dump = json.dumps(last_watched)
+            file.write(dump)
+    except Exception as e:
+        xbmc.log('Can\'t write '+LAST_WATCHED+'\n'+str(e),level=xbmc.LOGINFO)
+        traceback.print_exc()
+
+def load_search():
+    history = []
+    try:
+        if not os.path.exists(_profile):
+            os.makedirs(_profile)
+    except Exception as e:
+        traceback.print_exc()
+    
+    try:
+        with io.open(os.path.join(_profile, SEARCH_HISTORY), 'r', encoding='utf8') as file:
+            fdata = file.read()
+            try:
+                history = json.loads(fdata, "utf-8")
+            except TypeError:
+                history = json.loads(fdata)
+    except Exception as e:
+        traceback.print_exc()
+
+    return history
+    
+def store_search(what):
+    if what:
+        history = load_search()
+
+        if what in history:
+            history.remove(what)
+
+        history = [what] + history
+        
+        history = history[:MAX_HISTORY_SIZE]
+
+        try:
+            with io.open(os.path.join(_profile, SEARCH_HISTORY), 'w', encoding='utf8') as file:
+                try:
+                    data = json.dumps(history).decode('utf8')
+                except AttributeError:
+                    data = json.dumps(history)
+                file.write(data)
+        except Exception as e:
+            traceback.print_exc()
+
 def list_categories():
     xbmcplugin.setPluginCategory(_handle, _addon.getLocalizedString(30000))
     for category in CATEGORIES:
@@ -74,9 +159,18 @@ def list_categories():
     list_item.setInfo('video', {'title': _addon.getLocalizedString(30005),
                                 'genre': _addon.getLocalizedString(30005)})
     list_item.setArt({'icon': 'DefaultAddonsSearch.png'})
-    link = get_url(search='1')
+    link = get_url(search_menu='1')
     is_folder = True
     xbmcplugin.addDirectoryItem(_handle, link, list_item, is_folder)
+    
+    list_item = xbmcgui.ListItem(label=_addon.getLocalizedString(30008))
+    list_item.setInfo('video', {'title': _addon.getLocalizedString(30008),
+                                'genre': _addon.getLocalizedString(30008)})
+    list_item.setArt({'icon': 'DefaultAddonsUpdates.png'})
+    link = get_url(last='1')
+    is_folder = True
+    xbmcplugin.addDirectoryItem(_handle, link, list_item, is_folder)
+    
     xbmcplugin.endOfDirectory(_handle)
 
 def list_lists(category):
@@ -110,8 +204,9 @@ def process_items(posts, less, more):
     for post in posts:
         links = post.select('a') 
         if len(links) > 0:
-            print(post)
+            #print(post)
             img = post.select('img')[0]['src']
+            img = img.replace("1.jpg","default.jpg") # experimental - TODO Range:bytes=0-0 => 206 na overenie existencie?
             title_block = post.select('span')[0]
             title_link = links[0]
             href = title_link['href']
@@ -120,8 +215,9 @@ def process_items(posts, less, more):
             list_item = xbmcgui.ListItem(label=name)
             list_item.setInfo('video', {'title': name, 'plot': name})
             list_item.setArt({'thumb': img})
-            link = get_url(href=href)
-            is_folder = True
+            list_item.setProperty('IsPlayable', 'true')
+            link = get_url(href=href,title=name,img=img)
+            is_folder = False
             xbmcplugin.addDirectoryItem(_handle, link, list_item, is_folder)
 
     if more is not None:
@@ -172,13 +268,53 @@ def list_videos(category,order,page=None):
         process_items(posts, less, more)
 
     except Exception as e:
-        xbmc.log(str(e),level=xbmc.LOGNOTICE)
+        xbmc.log(str(e),level=xbmc.LOGINFO)
         traceback.print_exc()
         xbmcgui.Dialog().ok(_addon.getLocalizedString(30000), _addon.getLocalizedString(30001) + "\n" + str(e))
         xbmcplugin.endOfDirectory(_handle)
         return
 
     xbmcplugin.endOfDirectory(_handle, updateListing=update)
+
+def list_last_watched():
+    xbmcplugin.setPluginCategory(_handle, (_addon.getLocalizedString(30008)))
+
+    lw = load_last_watched()
+
+    for last in lw:
+        list_item = xbmcgui.ListItem(label=last["title"])
+        list_item.setInfo('video', {'title': last["title"], 'plot': last["title"]})
+        list_item.setArt({'thumb': last["img"]})
+        list_item.setProperty('IsPlayable', 'true')
+        link = get_url(href=last["href"],title=last["title"],img=last["img"])
+        is_folder = False
+        xbmcplugin.addDirectoryItem(_handle, link, list_item, is_folder)
+
+    xbmcplugin.endOfDirectory(_handle) #updateListing=update
+
+def search_menu():
+    xbmcplugin.setPluginCategory(_handle, (_addon.getLocalizedString(30005)))
+
+    xbmcplugin.setSetting(_handle, LAST_SEARCH, "")
+
+    list_item = xbmcgui.ListItem(label=_addon.getLocalizedString(30005))
+    list_item.setInfo('video', {'title': _addon.getLocalizedString(30005),
+                                'genre': _addon.getLocalizedString(30005)})
+    list_item.setArt({'icon': 'DefaultAddonsSearch.png'})
+    link = get_url(search='1')
+    is_folder = True
+    xbmcplugin.addDirectoryItem(_handle, link, list_item, is_folder)
+
+    history = load_search()
+    
+    for search in history:
+        listitem = xbmcgui.ListItem(label=search)
+        listitem.setArt({'icon': 'DefaultAddonsSearch.png'})
+        #commands = []
+        #commands.append(( _addon.getLocalizedString(30213), 'Container.Update(' + get_url(action='search',remove=search) + ')'))
+        #listitem.addContextMenuItems(commands)
+        xbmcplugin.addDirectoryItem(_handle, get_url(query=search), listitem, True)
+    xbmcplugin.endOfDirectory(_handle)
 
 def list_search(query=None, page=None):
     if page is not None:
@@ -190,10 +326,17 @@ def list_search(query=None, page=None):
 
     try:
         if query is None:
+            last = xbmcplugin.getSetting(_handle, LAST_SEARCH)
+            if "" != last:
+                query = last
+
+        if query is None:
             kb = xbmc.Keyboard('', _addon.getLocalizedString(30005))
             kb.doModal()
             if kb.isConfirmed():
                 query = kb.getText()
+                store_search(query)
+                xbmcplugin.setSetting(_handle, LAST_SEARCH, query)
             else:
                 query = ''
 
@@ -219,7 +362,7 @@ def list_search(query=None, page=None):
             process_items(posts, less, more)
 
     except Exception as e:
-        xbmc.log(str(e),level=xbmc.LOGNOTICE)
+        xbmc.log(str(e),level=xbmc.LOGINFO)
         traceback.print_exc()
         xbmcgui.Dialog().ok(_addon.getLocalizedString(30000), _addon.getLocalizedString(30001) + "\n" + str(e))
         xbmcplugin.endOfDirectory(_handle)
@@ -227,42 +370,53 @@ def list_search(query=None, page=None):
 
     xbmcplugin.endOfDirectory(_handle, updateListing=update)
 
-def list_streams(href):
+def streams_and_play(href,title,img):
     try:
         data_url = BASE_URL + href
         data_raw = _session.get(data_url, headers=HEADERS)
         data_text = data_raw.text
         html = BeautifulSoup(data_text, 'html.parser')
-
-        title = html.select('h3')[0].string
-        xbmcplugin.setPluginCategory(_handle, title)
-
-        plot = html.find_all('div', {'class' : 'm-t-10 overflow-hidden'}, True)[0].string
-
+        #title = html.select('h3')[0].string
+        plot = html.find_all('div', {'class' : 'm-t-10 overflow-hidden'}, True)[0].string.strip()
         video = html.find_all('video', {'id' : 'video'}, True)[0]
         sources = video.select('source')
+
+        cm = []
         for source in sources:
-            list_item = xbmcgui.ListItem(label=source['label'])
-            list_item.setInfo('video', {'title': title, 'plot': plot})
-            list_item.setArt({'thumb': video['poster']})
-            list_item.setProperty('IsPlayable', 'true')
-            link = source['src']
-            is_folder = False
-            xbmcplugin.addDirectoryItem(_handle, link, list_item, is_folder)
+            cm.append(_addon.getLocalizedString(30006) + " " + source['label'])
+        cm.append(_addon.getLocalizedString(30007))
+
+        result = xbmcgui.Dialog().contextmenu(cm)
+        if result == -1:
+            xbmcplugin.setResolvedUrl(_handle, True, xbmcgui.ListItem())
+            return
+        elif result == len(sources):
+            xbmcgui.Dialog().textviewer(title,plot)
+            xbmcplugin.setResolvedUrl(_handle, True, xbmcgui.ListItem())
+            return
+
+        source = sources[result]
+        list_item = xbmcgui.ListItem(label=source['label'], path=source['src'])
+        list_item.setInfo('video', {'title': title, 'plot': plot})
+        list_item.setArt({'thumb': video['poster']})
+        list_item.setProperty('IsPlayable', 'true')
+        link = source['src']
+        
+        store_last_watched(href, title, img)
+        
+        xbmcplugin.setResolvedUrl(_handle, True, list_item)
     except Exception as e:
-        xbmc.log(str(e),level=xbmc.LOGNOTICE)
+        xbmc.log(str(e),level=xbmc.LOGINFO)
         traceback.print_exc()
         xbmcgui.Dialog().ok(_addon.getLocalizedString(30000), _addon.getLocalizedString(30001) + "\n" + str(e))
         xbmcplugin.endOfDirectory(_handle)
         return
 
-    xbmcplugin.endOfDirectory(_handle)
-
 def router(paramstring):
     params = dict(parse_qsl(paramstring))
     if params:
         if 'href' in params:
-            list_streams(params['href'])
+            streams_and_play(params['href'],params['title'],params['img'])
         elif 'category' in params and 'order' in params and 'page' in params:
             list_videos(params['category'],params['order'],int(params['page']))
         elif 'category' in params and 'order' in params:
@@ -273,8 +427,12 @@ def router(paramstring):
             list_search(params['query'], int(params['page']))
         elif 'query' in params:
             list_search(params['query'])
+        elif 'search_menu' in params:
+            search_menu()
         elif 'search' in params:
             list_search()
+        elif 'last' in params:
+            list_last_watched()
         else:
             list_categories()
     else:
